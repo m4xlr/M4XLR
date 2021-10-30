76a2aa8426fe1aba80261ef5a4bf6a97d6632c8a
Max Landeo <maxlr2005@gmail.com>
2021-10-30 12:53:32 -0500

    created jouthweernal

---
+++ pages/jouthweernal/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../../components/navbar'
+import React from 'react'
+import Head from 'next/head'
+import Minfooter from '../../components/minfooter'
+
+export default function Jouthweernal (){
+    return(
+        <>
+        Hope this works :s
+        </>
+    )
+}
