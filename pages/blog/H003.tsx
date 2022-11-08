5f3c5a1edfe0725bf32fd7df8dc6e0cc03445df5
Max Landeo <maxlr2005@gmail.com>
2022-11-07 21:15:12 -0500

    fixing a typo in H003

--- pages/blog/H003.tsx
+++ pages/blog/H003.tsx
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
-import next from "next";
-import Hblog from '../../components/hblog'
+import Head from "next/head";
+import React, { Component } from "react"
 
 export default function H003(){
     return(
         <>
-        <Hblog title='Like a movie' content="Hagamos una prueba, un juego con la realidad
+        <Head>
+            <title>
+            Like a movie
+            </title>
+        </Head>
+        <main>
+            <span>
+            Hagamos una prueba, un juego con la realidad
         imaginemos por un momento que nuestra vida es una pelicula, imaginemos como comienza, como se desarrolla y como llega al momento en el que nos encontramos
         leyendo esto. 
         imaginemos que es una buena pelicula. y el desenlace de la misma depende de nosotros mismos
         no siempre sabremos que pasara luego pero podemos apuntarle a que hacer cuando eso pase
         en situaciones como esta la vida se convierte en algo lineal, pero recordemos que vivimos en un equilibrio de un sinfin de alternativas y solo depende de nosotros 
-        elegir que continua en la pelicula"/>
+        elegir que continua en la pelicula
+            </span>
+        </main>
         </>
     )
 }
