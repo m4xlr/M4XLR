a3b773d3b504509e95a1e6eacc308281dc5f952e
Max Landeo <maxlr2005@gmail.com>
2022-11-07 21:07:19 -0500

    ad id to scripts for ganalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,9 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
-    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+    <Script id='g-analytics-script' strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-    <Script strategy="lazyOnload">
+    <Script id='seekforinfo' strategy="lazyOnload">
     {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
